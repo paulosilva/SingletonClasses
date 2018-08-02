00233a917b1ff32be639e50a411f644656accd5a
Paulo Silva <silva.pmo@gmail.com>
2018-08-02 19:14:50 +0100

    add the AppTheme.swift

---
+++ AppTheme.swift
@@ -0,0 +1,39 @@
+//
+//  AppTheme.swift
+//  Paulo Silva
+//
+//  Created by Paulo Silva on 01/01/2018.
+//  Copyright © 2018 Paulo Silva. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct AppTheme {
+    
+    // Table View
+    
+    // Default Table View : Search Box - for Hide the box
+    let kDefaultTableViewSearchOffset: CGFloat = 56.0
+    
+    //let kDefaultTableViewHeaderHeight: CGFloat = 100
+    let kDefaultTableViewHeaderFont: String = "Avenir-Medium" //"HelveticaNeue" // "Avenir-Medium" // Change Font
+    let kDefaultTableViewHeaderFontSize: CGFloat = 20
+    let kDefaultTableViewHeaderFontColor: UIColor = .black
+    
+    // Default Table View
+    
+    //let kDefaultTableViewCellHeight: CGFloat = 50
+    let kDefaultTableViewCellFont: String = "Avenir-Medium" //"HelveticaNeue" // "Avenir-Medium" // Change Font
+    let kDefaultTableViewCellFontSize: CGFloat = 18
+    let kDefaultTableViewCellFontColor: UIColor = .black
+    
+    // Collection View
+    
+    // Default Collection View
+    //let kDefaultCollectionViewCellHeight: CGFloat = 50
+    let kDefaultCollectionViewCellFont: String = "Avenir-Medium" //"HelveticaNeue" // "Avenir-Medium" // Change Font
+    let kDefaultCollectionViewCellFontSize: CGFloat = 16
+    let kDefaultCollectionViewCellFontColor: UIColor = .black
+    
+}
